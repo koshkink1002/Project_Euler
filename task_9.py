e3a0e4f1535c5d601cc93e84b59d8633bba6eed0
koshkink1002 <koshkink1002@gmail.com>
2022-09-05 17:55:24 +0300

    Исправил орфографию.

--- task_9.py
+++ task_9.py
@@ -1,16 +1,16 @@
-def are_pythogorians(a: int, b: int, c: int) -> bool:
+def are_pythagorians(a: int, b: int, c: int) -> bool:
     if a ** 2 + b ** 2 == c ** 2:
         return True
     else:
         return False
 
 
-def show_pythogorians(limit: int):
+def show_pythagorians(limit: int):
     for a in range(1, limit):
         for b in range(1, limit):
             for c in range(1, limit):
-                if are_pythogorians(a, b, c):
+                if are_pythagorians(a, b, c):
                     print(a, b, c)
 
 
-show_pythogorians(20)
+show_pythagorians(20)
